447cdb8bf89ab1469175592e5badc0cea845bfdf
Lee-Junhee <jlee02@stuy.edu>
2019-03-07 01:10:59 -0500

    Added a comment

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -2,5 +2,5 @@ public class UserOfSpeakers {
     public static void main(String[] commandLine) {
 	Animal Goldilocks = new Retriever();
 	System.out.println(Animal.speak());
-    }
+    } //comment made by Junhee
 }
